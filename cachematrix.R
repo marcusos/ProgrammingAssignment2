7bc83ad639b581e0d8e0a17380952ece9a33c5bd
Marcus Oliveira <marcus.oliveira@blackapetech.com>
2014-12-21 15:49:56 -0200

    Created makeCacheMatrix, ,cacheSolve and testCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,70 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This script have a pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
-
+#This function creates a special "matrix" object that can cache its inverse., 
+#which is really  a list containing a function to
+#set the value of the vector
+#get the value of the vector
+#set the value of the mean
+#get the value of the mean
 makeCacheMatrix <- function(x = matrix()) {
-
+  #Inverse initialization
+  inv <- NULL
+  
+  #Get and Set for the matrix value
+  set <- function(y) {
+    #Setting matrix value
+    x <<- y
+    #Inverse re-initialization
+    inv <<- NULL
+  }
+  get <- function() x
+  
+  #Get and Set
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  
+  #Return the list to encapsulates the 'special' matrix
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+#This function computes the inverse of the special "matrix" returned by makeCacheMatrix 
+#above. If the inverse has already been calculated(and the matrix has not changed), 
+#then cacheSolve should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  #check if the inverse is cached and return it
+  inv <- x$getInverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  #Get the 'special' matrix value
+  data <- x$get()
+  
+  #Compute the inverse matrix and cache it
+  inv <- solve(data, ...)
+  x$setInverse(inv)
+  
+  #Return the inverse matrix
+  inv
 }
+
+#Function to test makeCacheMatrix and cacheSolve
+testCacheMatrix <- function() {
+  # 1-4 square matrix
+  m <- matrix(1:4, ncol = 2)
+  # special matrix initialization
+  cacheM <- makeCacheMatrix(m)
+  
+  print(cacheM);
+  
+  #compute inverse Matrix
+  inv <- cacheSolve(cacheM)
+  inv <- cacheSolve(cacheM)
+  
+  inv
+}
+
